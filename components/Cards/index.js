915faf1636e4abebe8292df2d95e21e85a7018fa
john <john@spinningplates.org>
2019-09-13 19:11:37 -0700

    completed Cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Cards(cardData) {
+  const card = document.createElement('div');
+  const headline = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const authorsName = document.createElement('span');
+
+  card.appendChild(headline);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  imgContainer.appendChild(img);
+  author.appendChild(authorsName);
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+
+  headline.textContent = cardData.headline;
+  img.src = cardData.authorPhoto;
+  authorsName.textContent = cardData.authorName;
+
+  return card;
+}
+
+const cardsContainer = document.querySelector('.cards-container');
+
+const makeCards = async url => {
+  try {
+    let res = await axios.get(url);
+    let articles = res.data.articles;
+    for (let key in articles) {
+      if (articles.hasOwnProperty(key)) {
+        articles[key].forEach(article => {
+          cardsContainer.appendChild(Cards(article));
+        });
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+makeCards('https://lambda-times-backend.herokuapp.com/articles');
