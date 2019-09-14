b7534b66a5b5e9bdfd59a8b2e2d27355ccc4e716
john <john@spinningplates.org>
2019-09-13 19:10:34 -0700

    completed Header and Tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function Tab(topic) {
+  const tab = document.createElement('div');
+  tab.classList.add('tab');
+  tab.textContent = topic;
+
+  return tab;
+}
+
+const topics = document.querySelector('.topics');
+
+const makeTab = async url => {
+  try {
+    let res = await axios.get(url);
+    res.data.topics.forEach(topic => {
+      topics.appendChild(Tab(topic));
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+makeTab('https://lambda-times-backend.herokuapp.com/topics');
